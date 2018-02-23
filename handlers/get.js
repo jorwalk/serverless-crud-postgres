98b16f4269089f3eb0dffa9250e18f402a5799a3
Jordan Walker <jorwalk@gmail.com>
2018-02-23 12:00:00 -0600

    get one

--- handlers/get.js
+++ handlers/get.js
@@ -1,12 +1,43 @@
 'use strict';
 const AWS = require('aws-sdk');
 
+// CONNECT TO POSTGRES
+const { Client } = require('pg')
+
+const client = new Client({
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DB,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT,
+});
+
 module.exports.pin = (event, context, callback) => {
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify({
-            message: 'Got Pin.'
-        })
-    };
-    callback(null, response);
+	context.callbackWaitsForEmptyEventLoop = false; 
+
+	client.connect()
+
+    const qry = "SELECT * FROM pins WHERE id = $1"
+    const vals = [ event.pathParameters.id ]
+
+    client.query(qry, vals, (err, res) => {
+        client.end()
+        let response = {}
+        
+        if(err) {
+        	response = {
+            	"statusCode": 400,
+            	"body": JSON.stringify({'success':false})
+        	};
+        } else {
+        	response = {
+            	"statusCode": 200,
+            	"body": JSON.stringify({'success':true, 'data': res.rows})
+        	};
+        }
+
+        callback(null, response);
+    })
+
 };
+
--- pin.sql
+++ pin.sql
@@ -1,18 +1,9 @@
-create domain latitude_t as double precision not null 
-                                             check(value>=-90 and value<=90);
-create domain longitude_t as double precision not null 
-                                              check(value>-180 and value<=180);
-
-create type geocoord_t as (latitude latitude_t, longitude longitude_t);
-
 CREATE TABLE pins (
   ID SERIAL PRIMARY KEY,
   title VARCHAR,
   subtitle VARCHAR,
   type INTEGER,
-  updated TIMESTAMP WITHOUT TIME ZONE
-  coords geocoord_t
+  lat      FLOAT,
+  long     FLOAT,
+  coord GEOGRAPHY(POINT)
 );
-
--- insert into my_table(geocoord) values ((31.778175,35.22995));
--- select id, (geocoord).* from my_table;
